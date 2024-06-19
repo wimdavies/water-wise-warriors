7da85120bbe192649fa34f2a28e4e85f932f75ba
wimdavies <william.johnston.davies@gmail.com>
2024-06-19 18:09:32 +0100

    Implement prototype controller and service that return a string

---
+++ api/src/main/java/com/water_wise_warriors/api/controller/BathingWaterController.java
@@ -0,0 +1,18 @@
+package com.water_wise_warriors.api.controller;
+
+import com.water_wise_warriors.api.service.BWQService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BathingWaterController {
+
+    @Autowired
+    private BWQService bwqService;
+
+    @GetMapping("/api/bathing-waters")
+    public String all() {
+        return bwqService.getBathingWaters().getBody();
+    }
+}
