d4c6f5b9b74e646e90402e3e47d36bbc6703824e
wimdavies <william.johnston.davies@gmail.com>
2024-06-19 12:58:05 +0100

    Project setup

---
+++ api/src/test/java/com/water_wise_warriors/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water_wise_warriors.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
