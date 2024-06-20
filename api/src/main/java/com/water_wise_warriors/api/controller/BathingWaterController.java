5126572d8688b6b6af155cda39cf46fbcfeb27b4
Shoaib Latif <shoaiblatif11@gmail.com>
2024-06-20 12:57:12 +0100

    Updated config package. added in WebConfig to resolve CORS issue

--- api/src/main/java/com/water_wise_warriors/api/controller/BathingWaterController.java
+++ api/src/main/java/com/water_wise_warriors/api/controller/BathingWaterController.java
@@ -3,6 +3,7 @@ package com.water_wise_warriors.api.controller;
 import com.water_wise_warriors.api.service.BWQService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
