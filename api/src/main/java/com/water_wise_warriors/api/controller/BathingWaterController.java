4fbe3a32a5a595f8d4c466397cd955be38d15a99
wimdavies <william.johnston.davies@gmail.com>
2024-06-20 10:51:58 +0100

    Change /api/bathing-waters to return the original BWQ response

--- api/src/main/java/com/water_wise_warriors/api/controller/BathingWaterController.java
+++ api/src/main/java/com/water_wise_warriors/api/controller/BathingWaterController.java
@@ -2,6 +2,7 @@ package com.water_wise_warriors.api.controller;
 
 import com.water_wise_warriors.api.service.BWQService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class BathingWaterController {
     private BWQService bwqService;
 
     @GetMapping("/api/bathing-waters")
-    public String all() {
-        return bwqService.getBathingWaters().getBody();
+    public ResponseEntity<String> all() {
+        return bwqService.getBathingWaters();
     }
 }
