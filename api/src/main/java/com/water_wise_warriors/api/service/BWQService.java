d0dae43274979e36f04c018108d0ab0fea52ad86
wimdavies <william.johnston.davies@gmail.com>
2024-06-21 12:50:54 +0100

    Moved RestClient configuration into BWQService for @RestClientTest unit tests to work

--- api/src/main/java/com/water_wise_warriors/api/service/BWQService.java
+++ api/src/main/java/com/water_wise_warriors/api/service/BWQService.java
@@ -2,11 +2,9 @@ package com.water_wise_warriors.api.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.water_wise_warriors.api.dto.BathingWaterDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -16,9 +14,15 @@ import java.util.List;
 @Service
 public class BWQService {
 
-    @Autowired
-    private RestClient restClient;
+    private final RestClient restClient;
 
+    public BWQService(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://environment.data.gov.uk/doc")
+                .build();
+    }
+
+    // TODO: add error handling for non-200 response codes
     public List<BathingWaterDTO> getBathingWaters() {
         ResponseEntity<String> response = restClient.get()
                 .uri("/bathing-water.json?_pageSize=1000")
