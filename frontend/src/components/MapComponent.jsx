46ecc01709157ca90c2dfebf489e0f7a0b325d00
wimdavies <william.johnston.davies@gmail.com>
2024-06-25 14:56:35 +0100

    Change order of LayerControl.Overlays, tidy up MapComponent

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -33,135 +33,62 @@ const LocateControl = () => {
     return null;
 };
 
-
-
-
 const MapComponent = () => {
-        const [bathingWaters, setBathingWatersData] = useState([]);
+    const [bathingWaters, setBathingWatersData] = useState([]);
 
-        useEffect(() => {
-            // Prepare your custom marker icon options
-            const customIcon = L.icon({
-                iconUrl: customMarkerIcon,
-                // iconRetinaUrl: customMarkerIcon2x,
-                // shadowUrl: customMarkerShadow,
-                iconSize: [25, 41], // Size of the icon
-                iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
-                shadowSize: [41, 41], // Size of the shadow
-                shadowAnchor: [12, 41], // Point of the shadow relative to the icon
-                popupAnchor: [1, -34] // Point from which the popup should open relative to the icon
-            });
-    
-            // Configure Leaflet's default icon options to use your custom icon
-            L.Marker.prototype.options.icon = customIcon;
+    // Classify data by compliance assessment
+    const excellentMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Excellent');
+    const goodMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Good');
+    const poorMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Poor');
 
-        }, []);
+    useEffect(() => {
+        // Prepare your custom marker icon options
+        const customIcon = L.icon({
+            iconUrl: customMarkerIcon,
+            // iconRetinaUrl: customMarkerIcon2x,
+            // shadowUrl: customMarkerShadow,
+            iconSize: [25, 41], // Size of the icon
+            iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+            shadowSize: [41, 41], // Size of the shadow
+            shadowAnchor: [12, 41], // Point of the shadow relative to the icon
+            popupAnchor: [1, -34] // Point from which the popup should open relative to the icon
+        });
 
-        useEffect(() => {
-            const fetchBathingWaters = async () => {
-                try {
-                    const response = await fetch('http://localhost:8080/api/bathing-waters'); 
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const data = await response.json(); //get items out of api
-                    console.log(data);
-                    setBathingWatersData(data); 
-                } catch (error) {
-                    console.error('Error fetching BathingWaters:', error);
+        // Configure Leaflet's default icon options to use your custom icon
+        L.Marker.prototype.options.icon = customIcon;
+
+    }, []);
+
+    useEffect(() => {
+        const fetchBathingWaters = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/api/bathing-waters'); 
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
                 }
-            };
+                const data = await response.json(); //get items out of api
+                console.log(data);
+                setBathingWatersData(data); 
+            } catch (error) {
+                console.error('Error fetching BathingWaters:', error);
+            }
+        };
 
-            fetchBathingWaters();
-            
+        fetchBathingWaters();
 
-        }, []); 
+    }, []); 
 
-        // Classify data by compliance assessment
-        const excellentMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Excellent');
-        const goodMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Good');
-        const poorMarkers = bathingWaters.filter(water => water.latestComplianceAssessment === 'Poor');
-    
-        return (
-            <section style={{ height: '80vh', width: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '0 auto' }}>
-                <MapContainer center={[52.727104, -1.62608]} zoom={6.5} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }} id='map'>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <LayersControl position="topright">
-                        <LayersControl.Overlay name="Excellent">
-                            <LayerGroup>                   
-                            {excellentMarkers.map((bathingWater) => (
-                            <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
-                                <Popup>
-                                    <div>
-                                        <h3>{bathingWater.name}</h3>
-                                        <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
-                                        <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
-                                        <p>
-                                        <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
-                                            Click here for more info and comments
-                                        </Link>
-                                        </p>
-                                        <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
-                                    </div>
-                                </Popup>
-                            </Marker>
-                            
-                        ))}
-                            </LayerGroup>
-                        </LayersControl.Overlay>
-
-                        <LayersControl.Overlay name="Good">
-                            <LayerGroup>                   
-                            {goodMarkers.map((bathingWater) => (
-                            <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
-                                <Popup>
-                                    <div>
-                                        <h3>{bathingWater.name}</h3>
-                                        <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
-                                        <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
-                                        <p>
-                                        <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
-                                            Click here for more info and comments
-                                        </Link>
-                                        </p>
-                                        <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
-                                    </div>
-                                </Popup>
-                            </Marker>
-                            
-                        ))}
-                            </LayerGroup>
-                        </LayersControl.Overlay>  
-                        
-                        <LayersControl.Overlay name="Poor">
-                            <LayerGroup>                   
-                            {poorMarkers.map((bathingWater) => (
-                            <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
-                                <Popup>
-                                    <div>
-                                        <h3>{bathingWater.name}</h3>
-                                        <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
-                                        <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
-                                        <p>
-                                        <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
-                                            Click here for more info and comments
-                                        </Link>
-                                        </p>
-                                        <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
-                                    </div>
-                                </Popup>
-                            </Marker>
-                            
-                        ))}
-                            </LayerGroup>
-                        </LayersControl.Overlay>  
-
-                        <LayersControl.Overlay name="Everything">
-                            <LayerGroup>                    
-                                {bathingWaters.map((bathingWater) => (
+    return (
+        <section style={{ height: '80vh', width: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '0 auto' }}>
+            <MapContainer center={[52.727104, -1.62608]} zoom={6.5} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }} id='map'>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <LayersControl position="topright">
+                    <LayersControl.Overlay name="All">
+                        <LayerGroup>
+                            {bathingWaters.map((bathingWater) => (
                                 <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
                                     <Popup>
                                         <div>
@@ -178,22 +105,79 @@ const MapComponent = () => {
                                     </Popup>
                                 </Marker>
                             ))}
-                            </LayerGroup>
-                        </LayersControl.Overlay>
+                        </LayerGroup>
+                    </LayersControl.Overlay>
+                    
+                    <LayersControl.Overlay name="Excellent">
+                        <LayerGroup>                   
+                            {excellentMarkers.map((bathingWater) => (
+                                <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
+                                    <Popup>
+                                        <div>
+                                            <h3>{bathingWater.name}</h3>
+                                            <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
+                                            <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
+                                            <p>
+                                            <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
+                                                Click here for more info and comments
+                                            </Link>
+                                            </p>
+                                            <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
+                                        </div>
+                                    </Popup>
+                                </Marker>
+                            ))}
+                        </LayerGroup>
+                    </LayersControl.Overlay>
 
-                        
-
-
-
-
-
-
-                    </LayersControl>
-
-                <LocateControl/>
-                </MapContainer>
-            </section>
-        );
+                    <LayersControl.Overlay name="Good">
+                        <LayerGroup>                   
+                            {goodMarkers.map((bathingWater) => (
+                                <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
+                                    <Popup>
+                                        <div>
+                                            <h3>{bathingWater.name}</h3>
+                                            <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
+                                            <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
+                                            <p>
+                                            <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
+                                                Click here for more info and comments
+                                            </Link>
+                                            </p>
+                                            <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
+                                        </div>
+                                    </Popup>
+                                </Marker>
+                            ))}
+                        </LayerGroup>
+                    </LayersControl.Overlay>  
+                    
+                    <LayersControl.Overlay name="Poor">
+                        <LayerGroup>                   
+                            {poorMarkers.map((bathingWater) => (
+                                <Marker key={bathingWater.eubwidNotation} position={[Number(bathingWater.lat), Number(bathingWater.lon)]}>
+                                    <Popup>
+                                        <div>
+                                            <h3>{bathingWater.name}</h3>
+                                            <p>Latest Compliance Assessment: {bathingWater.latestComplianceAssessment ? bathingWater.latestComplianceAssessment : "Information not available"}</p>
+                                            <p>Risk Prediction Level: {bathingWater.latestRiskPredictionLevel ? bathingWater.latestRiskPredictionLevel : "Information not available"}</p>
+                                            <p>
+                                            <Link to={`bathingwater/${bathingWater.eubwidNotation}`} state={{bathingWater:bathingWater}}>
+                                                Click here for more info and comments
+                                            </Link>
+                                            </p>
+                                            <p>Risk Expires At: {new Date(bathingWater.latestRiskPredictionExpiresAt).toLocaleDateString()}</p>
+                                        </div>
+                                    </Popup>
+                                </Marker>
+                            ))}
+                        </LayerGroup>
+                    </LayersControl.Overlay>  
+                </LayersControl>
+            <LocateControl/>
+            </MapContainer>
+        </section>
+    );
 }
-    
+
 export default MapComponent;
