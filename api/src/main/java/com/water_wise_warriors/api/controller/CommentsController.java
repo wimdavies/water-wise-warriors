fee3382d8e00832d3559d7efe73a6b19a296d6f8
hollycr <hollyclarerees@gmail.com>
2024-06-25 11:01:52 +0100

    fix comments controller annotation issue

--- api/src/main/java/com/water_wise_warriors/api/controller/CommentsController.java
+++ api/src/main/java/com/water_wise_warriors/api/controller/CommentsController.java
@@ -1,19 +1,13 @@
 package com.water_wise_warriors.api.controller;
 
-
 import com.water_wise_warriors.api.model.Comment;
 import com.water_wise_warriors.api.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CommentsController {
 
     @Autowired
@@ -26,10 +20,8 @@ public class CommentsController {
 
     @PostMapping("/api/{eubwid}/comments")
     public List<Comment> create(@PathVariable String eubwid, @RequestBody Comment comment) {
-        System.out.println("I am here");
         comment.setEubwid(eubwid);
         commentRepository.save(comment);
-
         return commentRepository.findByEubwidOrderByCreatedAtDesc(eubwid);
     }
 
